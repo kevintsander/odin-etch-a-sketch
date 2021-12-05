f09977a9a4de2e956088faf48ece30c7526912e6
kevintsander <kevintsander@gmail.com>
2021-12-05 00:24:23 -0500

    intial box layout setup

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function createSketchDivs(countPerSide) {
+    const easContainer = document.querySelector("#eas-container");
+
+    for(let i = 0; i < (countPerSide * countPerSide); i++) {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('eas-block');
+        //newDiv.addEventListener('hover', );
+
+        easContainer.appendChild(newDiv);
+    }
+}
+
+function setSketchBoxCount(countPerSide) {
+    const root = document.documentElement;
+    root.style.setProperty('--eas-box-per-side', countPerSide);
+    createSketchDivs(countPerSide);    
+}
+
+setSketchBoxCount(16);

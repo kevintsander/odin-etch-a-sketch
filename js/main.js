386a23067f9a55326e9428adac73a4fbb90f7e46
kevintsander <kevintsander@gmail.com>
2021-12-06 21:59:37 -0500

    cleaned up picker display

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function resetSketch() {
 function createSketchBoxes(countPerSide) {
     const easContainer = document.getElementById('eas-draw-container');
 
-    for(let i = 0; i < (countPerSide * countPerSide); i++) {
+    for (let i = 0; i < (countPerSide * countPerSide); i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('eas-block');
         newDiv.addEventListener('mouseover', e => {
@@ -41,7 +41,7 @@ function setSketchBoxColor(box) {
     if (sketchType === "rainbow") {
         let colorIncrement = 10;
 
-        if (typeof(_lastColor) === "undefined" || _lastColor + colorIncrement > 255) {
+        if (typeof (_lastColor) === "undefined" || _lastColor + colorIncrement > 255) {
             newColor = 0;
         }
         else {
@@ -58,23 +58,31 @@ function setSketchBoxColor(box) {
     _lastColor = newColor;
 }
 
-const sketchTypeButtons = document.querySelectorAll('.sketch-type');
 function setSketchType(newSketchType) {
-    sketchTypeButtons.forEach(button => {
-        if (button.getAttribute('data-sketch-type') == newSketchType) {
-            button.classList.add('active-sketch-type');
-        }
-        else {
-            button.classList.remove('active-sketch-type');
-        }
-    })
-    sketchType = newSketchType;
+    if (newSketchType != sketchType) {
+        sketchTypeButtons.forEach(button => {
+            if (button.getAttribute('data-sketch-type') == newSketchType) {
+                button.classList.add('active-sketch-type-button');
+            }
+            else {
+                button.classList.remove('active-sketch-type-button');
+            }
+        })
+        sketchType = newSketchType;
+    }
     console.log(`Sketch type: ${sketchType}`);
 }
 
+const sketchTypeButtons = document.querySelectorAll('.sketch-type-button');
 sketchTypeButtons.forEach(button => button.addEventListener('click', e => {
     setSketchType(e.target.getAttribute('data-sketch-type'));
 }));
+
+const easPickerSideCount = document.getElementById('eas-picker-side-count');
+easPickerSideCount.addEventListener('mouseup', e=> {
+    const easPickerValue = document.getElementById('eas-picker-value');
+    easPickerValue.textContent = easPickerSideCount.value;
+})
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', e => {
